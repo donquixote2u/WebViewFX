8beedcc5c9cc246e2a9d92075d3dec9adc952cce
donquixote2u <donquixote2u@gmail.com>
2018-04-14 14:03:04 +1200

    Initial commit

---
+++ app/src/androidTestDebug/aidl/com/bvw/webviewmkt/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.bvw.webviewmkt;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by bruce on 28/10/17.
+ */
+public class MainActivityTest extends TestCase {
+
+}
---
+++ app/src/main/java/com/example/webviewmkt/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.webviewmkt;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://sharenet.otumoetaitennis.co.nz/shares/MktSnapshot1.php");
+    }
+	
+}
